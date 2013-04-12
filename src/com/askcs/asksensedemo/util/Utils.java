705ea45f14a119556b133303e18d8c8e9a0e2560
Bart Kiers <bkiers@gmail.com>
2013-04-12 22:29:31 +0200

    Added some JavaDocs.

--- src/com/askcs/asksensedemo/util/Utils.java
+++ src/com/askcs/asksensedemo/util/Utils.java
@@ -6,33 +6,45 @@ import java.security.NoSuchAlgorithmException;
 
 import android.util.Log;
 
+/**
+ * Some static utility methods.
+ */
 public final class Utils {
-	
-	private static final String TAG = Utils.class.getName();
-	
-	private Utils() {
-		// No need to instantiate this class.
-	}
-	
-	public static String md5(String input) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			byte[] messageDigest = md.digest(input.getBytes());
-			BigInteger number = new BigInteger(1, messageDigest);
-			String md5 = number.toString(16);
-		 
-			while (md5.length() < 32) {
-				md5 = "0" + md5;
-			}
-		 
-			return md5;
-		} catch (NoSuchAlgorithmException e) {
-			Log.e(TAG, e.getLocalizedMessage());
-			return null;
-		}
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(Utils.md5("!Amk220Bk284"));
-	}
+
+    private static final String TAG = Utils.class.getName();
+
+    private Utils() {
+        // No need to instantiate this class.
+    }
+
+    /**
+     * Creates an MD5 hash from a given string.
+     *
+     * @param input
+     *         the string to generate the MD% hash from.
+     *
+     * @return an MD5 hash from a given string.
+     *
+     * @throws RuntimeException
+     *         if the MD5 hashing algorithm is not
+     *         available (very unlikely though).
+     */
+    public static String md5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String md5 = number.toString(16);
+
+            while (md5.length() < 32) {
+                md5 = "0" + md5;
+            }
+
+            return md5;
+        }
+        catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
