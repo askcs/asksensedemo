b059200417dd64c1d970caae3025c4542de429cb
Bart Kiers <bkiers@gmail.com>
2013-04-12 21:18:44 +0200

    Changed radio button in checkboxes.

--- src/com/askcs/asksensedemo/MainActivity.java
+++ src/com/askcs/asksensedemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import com.askcs.asksensedemo.database.DatabaseHelper;
@@ -30,7 +32,6 @@ public class MainActivity extends Activity {
 
         try {
             final Setting userSetting = dao.queryForId(Setting.USER_KEY);
-            final Setting passwordSetting = dao.queryForId(Setting.PASSWORD_KEY);
 
             Log.d(TAG, "userSetting=" + userSetting);
 
@@ -48,14 +49,11 @@ public class MainActivity extends Activity {
 
         super.setContentView(R.layout.main);
 
-        init(R.id.id_radio_group_activity, R.id.id_radio_activity_enabled,
-                R.id.id_radio_activity_disabled, Setting.ACTIVITY_ENABLED_KEY);
+        init(R.id.id_checkbox_activity, Setting.ACTIVITY_ENABLED_KEY);
 
-        init(R.id.id_radio_group_location, R.id.id_radio_location_enabled,
-                R.id.id_radio_location_disabled, Setting.LOCATION_ENABLED_KEY);
+        init(R.id.id_checkbox_location, Setting.LOCATION_ENABLED_KEY);
 
-        init(R.id.id_radio_group_presence, R.id.id_radio_presence_enabled,
-                R.id.id_radio_presence_disabled, Setting.PRESENCE_ENABLED_KEY);
+        init(R.id.id_checkbox_presence, Setting.PRESENCE_ENABLED_KEY);
     }
 
     @Override
@@ -71,28 +69,24 @@ public class MainActivity extends Activity {
         }
     }
 
-    private void init(int groupId, int enabledId, int disabledId, final String key) {
+    private void init(int checkboxId, final String key) {
 
         final Dao<Setting, String> dao = this.getHelper().getSettingDao();
 
-        final RadioGroup group = (RadioGroup)super.findViewById(groupId);
-        final RadioButton enabled = (RadioButton)super.findViewById(enabledId);
-        final RadioButton disabled = (RadioButton)super.findViewById(disabledId);
+        final CheckBox checkBox = (CheckBox)super.findViewById(checkboxId);
 
         try {
 
             final Setting enabledSetting = dao.queryForId(key);
 
-            // Initialize the radio buttons.
-            enabled.setChecked(Boolean.valueOf(enabledSetting.getValue()));
-            disabled.setChecked(!Boolean.valueOf(enabledSetting.getValue()));
+            checkBox.setChecked(Boolean.valueOf(enabledSetting.getValue()));
 
-            group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            checkBox.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                public void onClick(View v) {
                     try {
-                        // Upon a radio button change, update the setting in the local DB.
-                        String newValue = enabled.isChecked() ? String.valueOf(Boolean.TRUE) : String.valueOf(Boolean.FALSE);
+                        // Upon change, update the setting in the local DB.
+                        String newValue = checkBox.isChecked() ? String.valueOf(Boolean.TRUE) : String.valueOf(Boolean.FALSE);
                         enabledSetting.setValue(newValue);
                         Log.d(TAG, "update setting: " + enabledSetting);
                         dao.update(enabledSetting);
