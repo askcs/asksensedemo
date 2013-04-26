8a0441fe2a0a8fcc42fc51e2cf8597a8376c7cae
Bart Kiers <bkiers@ask-cs.com>
2013-04-26 13:37:40 +0200

    Added a unit test.

---
+++ src/test/java/com/askcs/asksensedemo/util/UtilsTest.java
@@ -0,0 +1,11 @@
+package com.askcs.asksensedemo.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bart
+ * Date: 4/26/13
+ * Time: 12:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UtilsTest {
+}
